baa7ebfce670e30ba495edb55b745cd9748090ce
Viviana Benitez <magi61012@gmail.com>
2020-01-23 18:20:03 +0000

    refactor code and add tests

--- src/main/java/SupermarketQueue.java
+++ src/main/java/SupermarketQueue.java
@@ -2,52 +2,40 @@ import java.util.Arrays;
 
 public class SupermarketQueue {
 
-    public static int queueTime(int[] customes, int numberTill){
+    public static int queueTime (int[] customers, int numberTill) {
 
-        int waitTime=0;
 
-        if(customes.length==0){
-            return 0;
-        }
+        if (customers.length == 0) return 0;
 
-        int res1=customes[0];
+        int waitTime;
 
-        if(numberTill>customes.length){
-            for (int i = 0; i < customes.length; i++) {
-                if (res1<customes[i]){
-                    res1=customes[i];
+
+        waitTime = customers[0];
+        if (numberTill > customers.length) {
+            for (int i = 0; i < customers.length; i++) {
+                if (waitTime < customers[i]) {
+                    waitTime = customers[i];
                 }
-            }
-            return res1;
-        }
-
-
-
-
-        if(numberTill==1  || numberTill==0 && customes.length!=0){
-            for (int i = 0; i <customes.length; i++) {
-                waitTime+=customes[i];
             }
             return waitTime;
         }
 
-        if (customes.length==1){
-            if(numberTill==1){
-                return customes[0];
-            }else{
-                return customes[0];
-            }
 
+        waitTime=0;
+        if(numberTill==1  || numberTill==0){
+            for (int i = 0; i <customers.length; i++) {
+                waitTime+=customers[i];
+            }
+            return waitTime;
         }
 
+        if (customers.length==1) return customers[0];
 
 
-        int result=0;
 
-        int[] count= Arrays.copyOfRange(customes,0,numberTill);
-        int[] sumCount= Arrays.copyOfRange(customes,0,numberTill);
-        int[] count2= Arrays.copyOfRange(customes,numberTill,customes.length);
-        int res=count[0];
+
+        int[] sumCount= Arrays.copyOfRange(customers,0,numberTill);
+        int[] count2= Arrays.copyOfRange(customers,numberTill,customers.length);
 
 
         for (int j = 0; j < count2.length; j++) {
@@ -60,21 +48,15 @@ public class SupermarketQueue {
                 }
             }
 
-            count[aux]=count2[j];
-                /*for (int i = 0; i < count.length; i++) {
-
-                }*/
             sumCount[aux]+=count2[j];
         }
 
+        waitTime=0;
         for (int i = 0; i < sumCount.length; i++) {
-            if (result<sumCount[i]){
-                result=sumCount[i];
+            if (waitTime<sumCount[i]){
+                waitTime=sumCount[i];
             }
         }
-
-
-        return result;
-
+        return waitTime;
     }
 }
--- src/test/java/SupermarketQueue_.java
+++ src/test/java/SupermarketQueue_.java
@@ -6,14 +6,34 @@ public class SupermarketQueue_ {
 
     @Test
     public void if_there_are_not_customers_return_0(){
-        assertEquals(SupermarketQueue.queueTime(new int[0], 1), 0);
+        int [] vector=new int[0];
+        assertEquals(SupermarketQueue.queueTime(vector, 1), 0);
     }
 
     @Test
-    public void test(){
+    public void only_till_return_sum_of_waitTime_customers(){
         int[] vector=new int[]{5,3,4};
         assertEquals(SupermarketQueue.queueTime(vector, 1), 12);
     }
+
+    @Test
+    public void number_till_is_greater_than_number_customers(){
+        int[] vector=new int[]{5,3,4};
+        assertEquals(SupermarketQueue.queueTime(vector, 100), 5);
+    }
+
+    @Test
+    public void only_customer(){
+        int[] vector=new int[]{5};
+        assertEquals(SupermarketQueue.queueTime(vector, 100), 5);
+    }
+
+    @Test
+    public void number_till_is_0(){
+        int[] vector=new int[]{5,10,2,4,5};
+        assertEquals(SupermarketQueue.queueTime(vector, 0), 26);
+    }
+    
 
 
 }
